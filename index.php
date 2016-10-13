9df7b7d9b9fe542fafc3a87f136530f0562969d4
prithviraj <prithviraj.en@gmail.com>
2016-10-13 22:12:22 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+	require "twitter.php";
+	$client=new tweet_API();
+	$tweets=$client->search_by_hashtag("#custserv");
+?>
+
+<head>
+	<meta charset="utf-8">      
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+	<style type="text/css">
+		body{
+			background-color: #d9edf7;
+		}
+	    .tweet{
+        border-radius: 4px 4px 4px 4px;
+		-moz-border-radius: 4px 4px 4px 4px;
+		-webkit-border-radius: 4px 4px 4px 4px;
+		border: 0px solid #000000;
+        -webkit-box-shadow: 8px 8px 8px -5px rgba(0,0,0,0.75);
+		-moz-box-shadow: 8px 8px 8px -5px rgba(0,0,0,0.75);
+		box-shadow: 8px 8px 8px -5px rgba(0,0,0,0.75);
+		background-color: #1cb7e6;
+		margin-top: 8px;
+		margin-bottom: 8px;
+      }
+      p{
+      	text-align: right;
+      }
+    </style>
+	<title>Tweets</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="container">
+			<h1>Popular Tweets with #custserv</h1>
+		</div>
+		<div class="container">
+			<?php
+				foreach($tweets as $tweet){
+					echo "<div class=\"container tweet\">"."<h3>".$tweet["text"]."</h3>"."<p>".$tweet["user_name"]."</p>"."</div>";
+				}
+			?>			
+		</div>
+	</div>
+</body>
+</html>
---
+++ twitter.php
@@ -0,0 +1,42 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+class tweet_API{
+
+	//the oauth connection handler 
+	private $connection;
+
+	//twitter app specific keys
+	private $access_token="3368069441-HiKfV6cEdTOeI4gFZbcKDgbIsoa3DweBiZ5BRZk";
+	private $access_token_secret="iTu358VxlYsGJDxInLd1TnR7igs2gFDWMa2svHXFLgxzo";
+	private $consumer_key="qUJP0QiullM4pQ2eek3ThQjdV";
+	private $consumer_secret="ZsaPB14pWoWtG0XrsENdMQL62rbNqA4MAqBMJGOOYBbihV0lXI";
+
+	//class constructor
+	public function tweet_API() {
+		$this->connection = new TwitterOAuth($this->consumer_key,$this->consumer_secret,$this->access_token,$this->access_token_secret);
+		$this->connection->setTimeouts(10, 25);
+	}
+
+	public function search_by_hashtag($tag="#custserv")
+	{
+		//result_type can be popular, mixed OR recent(default)
+		$query = array(
+		  "q" => $tag,
+		  "result_type" => "popular",
+		  "count" => "100"
+		);
+		$content = $this->connection->get('search/tweets', $query);
+		$tweets=array();
+		foreach ($content->statuses as $result) {
+			if($result->retweet_count > 0){
+				//echo $result->user->screen_name;
+				$tweets[]=array("user_name"=>$result->user->screen_name,"text"=>$result->text);
+			}
+		}
+		return $tweets;
+	}
+}
